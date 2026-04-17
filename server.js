e1187290f6494f09b6e64129809b4f6d8335ccc7
dchaschin79-sys <dchaschin79@gmail.com>
2026-04-16 23:58:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-:::writing{variant=“standard” id=“srv01”}
-const http = require(‘http’);
-const fs = require(‘fs’);
+const http = require('http');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-fs.readFile(‘index.html’, (err, data) => {
-if (err) {
-res.writeHead(500);
-return res.end(‘Error loading page’);
-}
-res.writeHead(200, { ‘Content-Type’: ‘text/html’ });
-res.end(data);
-});
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading page');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
-server.listen(port, ‘0.0.0.0’, () => {
-console.log(Server running on port ${port});
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
-:::
